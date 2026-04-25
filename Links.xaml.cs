50aa56d4ea2515b4711bde72110070035592789c
Peter Ogdin <pcogdin94@gmail.com>
2026-04-25 11:45:05 -0400

    Add files via upload

--- Links.xaml.cs
+++ Links.xaml.cs
@@ -7,26 +7,37 @@ public partial class Links : ContentPage
         InitializeComponent();
     }
 
+    // UCOR Site
+    private async void OnUcorSiteClicked(object sender, EventArgs e)
+    {
+        await Browser.OpenAsync("https://umbra-site-five.vercel.app/");
+    }
+
+    // Discord
     private async void OnDiscordClicked(object sender, EventArgs e)
     {
         await Browser.OpenAsync("https://www.discord.gg/dTxh6rauzM");
     }
 
+    // RSI
     private async void OnRSIClicked(object sender, EventArgs e)
     {
         await Browser.OpenAsync("https://www.robertsspaceindustries.com/en/orgs/UCOR");
     }
 
+    // Spectrum
     private async void OnSpectrumClicked(object sender, EventArgs e)
     {
         await Browser.OpenAsync("https://www.robertsspaceindustries.com/spectrum/community/UCOR");
     }
 
+    // Erkul
     private async void OnErkulClicked(object sender, EventArgs e)
     {
         await Browser.OpenAsync("https://www.erkul.games/live/ships");
     }
 
+    // SP Viewer
     private async void OnSPViewerClicked(object sender, EventArgs e)
     {
         await Browser.OpenAsync("https://www.spviewer.eu/");
